752f53e669e086a33efe56dd152c643e84cb4a18
Matt Stauffer <matt@tighten.co>
2020-02-09 17:28:20 -0500

    Add Spatie Query Builder for eager loading

--- app/Http/Controllers/API/ArticleController.php
+++ app/Http/Controllers/API/ArticleController.php
@@ -6,11 +6,16 @@ use App\Article;
 use App\Http\Controllers\Controller;
 use App\Transformers\ArticleTransformer;
 use Illuminate\Http\Request;
+use Spatie\QueryBuilder\QueryBuilder;
 
 class ArticleController extends Controller
 {
     public function index()
     {
-        return fractal(Article::all(), new ArticleTransformer)->respond();
+        $articles = QueryBuilder::for(Article::class)
+            ->allowedIncludes(['author', 'comments'])
+            ->get();
+
+        return fractal($articles, new ArticleTransformer)->respond();
     }
 }
