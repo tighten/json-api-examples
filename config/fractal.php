25c76f66ddc459b90fa63fe163c4883ee85cfb48
Matt Stauffer <matt@tighten.co>
2020-02-10 15:26:09 -0500

    Switch to JSON API serializer

--- config/fractal.php
+++ config/fractal.php
@@ -6,7 +6,7 @@ return [
      * may be left empty to use Fractal's default one. This can either be a
      * string or a League\Fractal\Serializer\SerializerAbstract subclass.
      */
-    // 'default_serializer' => \League\Fractal\Serializer\JsonApiSerializer::class,
+    'default_serializer' => \League\Fractal\Serializer\JsonApiSerializer::class,
 
     /* The default paginator to be used when performing a transformation. It
      * may be left empty to use Fractal's default one. This can either be a
